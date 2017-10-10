3065d1ad03e76feaef5b3430f9b887c9aa24a4f3
leejiaolong <theonejiaolong@163.com>
2017-10-10 16:15:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,25 @@ import os
 import json
 import sys
 import requests
+import datetime
 import re
+#import logging
+#logging.basicConfig(level=logging.DEBUG)
 from multiprocessing import Pool
-from user_obj import User
+requests.packages.urllib3.disable_warnings()
 __auther__ = 'heishan'
 
-userOne = User('','')
-userTwo = User('','')
-user_pool = [userOne,userTwo]
+class User(object):
+    def __init__(self,userName,password):
+        self.userName = userName
+        self.password = password
+    def __str__(self):
+        return str('userName=%s,password=%s' % (self.userName,self.password))
 
+userOne = User('180....','//...')
+userTwo = User('153....','z.....')
+userThree = User('13....','......')
+user_pool = [userOne,userTwo,userThree]
 class SMZDMDailyException(Exception):
     def __init__(self, req):
         self.req = req
@@ -21,7 +31,7 @@ class SMZDMDailyException(Exception):
         return str(self.req)
 
 class SMZDMAutoSign(object):
-    BASE_URL = 'http://zhiyou.smzdm.com'
+    BASE_URL = 'https://zhiyou.smzdm.com'
     LOGIN_URL = BASE_URL + '/user/login/ajax_check'
     CHECKIN_URL = BASE_URL + '/user/checkin/jsonp_checkin'
 
@@ -30,11 +40,11 @@ class SMZDMAutoSign(object):
         self.password = password
         self.session = requests.Session()
 
-    def checkin(self):
+    def login(self):
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:20.0) Gecko/20100101 Firefox/20.0',
             'Host': 'zhiyou.smzdm.com',
-            'Referer': 'http://www.smzdm.com/'
+            'Referer': 'https://www.smzdm.com/'
         }
 
         params = {
@@ -43,32 +53,49 @@ class SMZDMAutoSign(object):
         }
 
         r = self.session.get(self.BASE_URL, headers=headers, verify=False)
-        r = self.session.post(self.LOGIN_URL, data=params, headers=headers, verify=False)
+        r = self.session.post(self.LOGIN_URL, data=params, headers=headers)
+        if r.status_code != 200:
+            raise SMZDMDailyException(r)
+        data = r.text
+        jdata = json.loads(data)
+        if jdata["error_code"] == 0:
+            print ("%s登录成功" % self.username)
+            return self.auto_ceck()
+        else:
+            print ("%s登录失败" % self.username)
+            return "%s签到失败" % self.username
+
+    def auto_ceck(self):
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:20.0) Gecko/20100101 Firefox/20.0',
+            'Host': 'zhiyou.smzdm.com',
+            'Referer': 'https://www.smzdm.com/'
+        }
         r = self.session.get(self.CHECKIN_URL, headers=headers, verify=False)
         if r.status_code != 200:
             raise SMZDMDailyException(r)
-
         data = r.text
         jdata = json.loads(data)
+        if jdata["error_code"] == 0:
+            result = jdata["data"]
+            return "账户:%s增加积分:%s,金币%s,总积分:%s,等级:%s" % (self.username,result["add_point"],result["gold"],result["point"],result["rank"])
+        else:
+            return "签到失败了"
 
-        return jdata
 def long_time_task(user):
-    print('Begin stak at %s' % user.userName)
     if user.userName and user.password:
         try:
             smzdm = SMZDMAutoSign(user.userName, user.password)
-            result = smzdm.checkin()
+            result = smzdm.login()
         except SMZDMDailyException as e:
             print('fail', e)
         except Exception as e:
             print('fail', e)
-
-        print('success', result)
+        nowtime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print('签到信息: %s,签到时间:%s' % (result,nowtime))
     else:
         print('SMZDM_USERNAME and SMZDM_PASSWORD can not  be empty ')
         sys.exit()
-    print('end stak at %s' % user.userName)
-
 if __name__ == '__main__':
     p = Pool()
     for user in user_pool:
